9ab2207330d20612f85663f28e8bd444a62c260d
wleferrand <william@corefarm.com>
2011-02-04 14:29:58 -0800

    adding firm followers

--- src/retrieve.ml
+++ src/retrieve.ml
@@ -53,16 +53,60 @@ let extract_string_of_tag_all tag s =
   
   internal_loop [] i
 
-(* Mem a tag *)
-let mem_tag_of_string tag s = 
+
+(* Get an element *)
+
+let get_attribute name s = 
   let i = Xmlm.make_input (`String (0, s)) in
   catch 
+    (fun () -> 
+      let rec goto_name i = 
+	lwt __input = Xmlm.input i in 
+        match __input with 
+	  | `El_start ((_, local), _) when local = "Name" -> check_name i
+	  | _ -> goto_name i 
+     and check_name i = 
+      lwt __input = Xmlm.input i in 
+      match __input with 
+	| `Data d when d = name -> goto_value i
+	| _ -> goto_name i  
+     and goto_value i = 
+     lwt __input = Xmlm.input i in 
+     match __input with
+       | `El_start ((_, local), _) when local = "Value" -> extract_value i 
+       | _ -> goto_value i 
+and extract_value i =
+    lwt __input = Xmlm.input i in 
+     match __input with 
+       | `Data d -> return (Some d) 
+       | _ -> extract_value i in
+   goto_name i)
+    (fun _ -> return None)
+
+(* Mem a tag *)
+exception Missing 
+
+let fail_if_tag tag s = 
+  let i = Xmlm.make_input (`String (0, s)) in
+  catch  
     (fun () -> 
       let rec loop_over_xml i = 
 	lwt __input = Xmlm.input i in 
         match __input with 
-	  | `El_start ((_, local), _) when local = tag -> return true 
+	  | `El_start ((_, local), _) when local = tag -> fail Missing
+	  | _ -> loop_over_xml i in 
+        loop_over_xml i >>= fun _ -> return s)
+    (function Missing -> fail Missing | _ -> return s)
+  
+let mem_tag tag s = 
+  let i = Xmlm.make_input (`String (0, s)) in
+   catch  
+    (fun () -> 
+      let rec loop_over_xml i = 
+	lwt __input = Xmlm.input i in
+        match __input with 
+	  | `El_start ((_, local), _) when local = tag -> return true
 	  | _ -> loop_over_xml i in 
         loop_over_xml i)
-    (fun _ -> return false)
+    (function e -> return false)
   
